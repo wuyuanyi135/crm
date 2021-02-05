92c9c62d082166f180935bf31855bc7b65a4e19d
wuyuanyi <wuyuanyi135@gmail.com>
2021-02-04 23:54:43 -0500

    test 2d polymorphic case

--- crm/presets/hypothetical.py
+++ crm/presets/hypothetical.py
@@ -106,17 +106,34 @@ class Hypothetical2D(SystemSpec):
     )]
 
 
-class HypotheticalPolymorphic2D(SystemSpec):
+class HypotheticalPolymorphicEqualGrowth2D(SystemSpec):
     forms = [
         ParametricFormSpec(
             name="alpha",
             density=1.54,
             solubility_coefs=np.array([4.564e-3, 3.032e-5, 8.437e-6]),
-            g_coefs=np.array([0.1e-6, 0.15e-6]),
-            g_powers=np.array([1, 1.2]),
-            d_coefs=np.array([2.2e-6, 2.1e-6]),
-            d_powers=np.array([1, 1.1]),
+            g_coefs=np.array([0.1e-6, 0.1e-6]),
+            g_powers=np.array([1, 1]),
+            d_coefs=np.array([2.2e-6, 2.2e-6]),
+            d_powers=np.array([1, 1]),
             pn_coef=1e8,
+            pn_power=2,
+            pn_ke=0,
+            sn_coef=1e10,
+            sn_power=2,
+            sn_vol_power=2 / 3,
+            shape_factor=0.48,
+            volume_fraction_powers=np.array([2, 1])
+        ),
+        ParametricFormSpec(
+            name="beta",
+            density=1.54,
+            solubility_coefs=np.array([6.222e-3, -1.165e-4, 7.644e-6]),
+            g_coefs=np.array([0.1e-6, 0.1e-6]),
+            g_powers=np.array([1, 1]),
+            d_coefs=np.array([2.2e-6, 2.2e-6]),
+            d_powers=np.array([1, 1]),
+            pn_coef=1e7,
             pn_power=2,
             pn_ke=0,
             sn_coef=1e10,
--- tests/test_mcsolver.py
+++ tests/test_mcsolver.py
@@ -5,7 +5,7 @@ from crm.base.input import ConstTemperatureInput
 from crm.base.output_spec import OutputLastSpec
 from crm.mcsolver import MCSolver, MCState, MCSolverOptions
 from crm.presets.hypothetical import Hypothetical1D, HypotheticalPolymorphic1D, Hypothetical2D, \
-    HypotheticalEqualGrowth2D
+    HypotheticalEqualGrowth2D, HypotheticalPolymorphicEqualGrowth2D
 
 
 def test_simple():
@@ -37,23 +37,29 @@ def test_polymorphic():
                 assert np.all(n[:, 0] >= 0), f"form {i} contains negative dimension"
 
 
-def test_2d():
-    options = MCSolverOptions(attach_extra=True, profiling=True)
-    system_spec = Hypothetical2D()
-    concentration = system_spec.forms[0].solubility(60)
-    state = system_spec.make_empty_state(concentration=concentration, temperature=25)
-
-    solver = MCSolver(system_spec, options)
-    input_ = ConstTemperatureInput(25.)
-    state_output = solver.compute(state, 3600, input_)
-
-    assert True
-
-
 def test_equal_growth_2d_should_match_1d():
     options = MCSolverOptions(attach_extra=True, profiling=True, output_spec=OutputLastSpec())
     system_spec_2d = HypotheticalEqualGrowth2D()
     system_spec_1d = Hypothetical1D()
+    concentration = system_spec_2d.forms[0].solubility(60)
+
+    state = system_spec_2d.make_empty_state(concentration=concentration, temperature=25)
+    solver2d = MCSolver(system_spec_2d, options)
+    input_ = ConstTemperatureInput(25.)
+    state_output_2d = solver2d.compute(state, 3600, input_)
+
+    state = system_spec_1d.make_empty_state(concentration=concentration, temperature=25)
+    solver1d = MCSolver(system_spec_1d, options)
+    input_ = ConstTemperatureInput(25.)
+    state_output_1d = solver1d.compute(state, 3600, input_)
+
+    assert np.isclose(state_output_1d[-1].concentration, state_output_2d[-1].concentration)
+
+
+def test_equal_growth_2d_polymorph_match_1d_polymorph():
+    options = MCSolverOptions(attach_extra=True, profiling=True, output_spec=OutputLastSpec())
+    system_spec_2d = HypotheticalPolymorphicEqualGrowth2D()
+    system_spec_1d = HypotheticalPolymorphic1D()
     concentration = system_spec_2d.forms[0].solubility(60)
 
     state = system_spec_2d.make_empty_state(concentration=concentration, temperature=25)
