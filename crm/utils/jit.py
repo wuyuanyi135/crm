b8048cd759faff02019b35d5129f3b6ad6b92d37
wuyuanyi <wuyuanyi135@gmail.com>
2021-02-11 21:34:47 -0500

    agglomeration may result in very high number of rows. Hence the jit compression is implemented for compressed result or even compressed result as input (like grid method )

--- crm/utils/jit.py
+++ crm/utils/jit.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from numba import jit, prange
+from numba import jit, prange, typed
 import numpy as np
 
 
@@ -76,7 +76,7 @@ def volume_average_size_jit(n: np.ndarray, volume_fraction_powers: np.ndarray, s
         return ret
 
 
-@jit(nopython=True, cache=True)
+@jit(nopython=True, cache=True, fastmath=True, parallel=True)
 def partition_equivalent_rows_jit(ns, volume_fraction_powers: np.ndarray, shape_factor: float) -> np.ndarray:
     ncols = ns[0].shape[1]
     nrows = len(ns)
@@ -93,8 +93,9 @@ def partition_equivalent_rows_jit(ns, volume_fraction_powers: np.ndarray, shape_
     return ret
 
 
-@jit(nopython=True, cache=True)
-def binary_agglomeration_jit(n, alpha: float, volume_fraction_powers: np.ndarray, shape_factor: float):
+@jit(nopython=True, cache=True, nogil=True)
+def binary_agglomeration_jit(n, alpha: float, volume_fraction_powers: np.ndarray, shape_factor: float,
+                             combination_index_=None, combination_table_=None, compression=False):
     """
     Constant binary agglomeration
     TODO: implement agglomeration along different dimension
@@ -104,18 +105,23 @@ def binary_agglomeration_jit(n, alpha: float, volume_fraction_powers: np.ndarray
     """
     nrows = n.shape[0]
     ncols = n.shape[1]
-
-    combination_index = np.triu_indices(nrows, 1)
+    if combination_index_ is None:
+        combination_index: tuple = np.triu_indices(nrows, 0)  # combination can self-intersect!
+    else:
+        combination_index = combination_index_
     # (n choose 2, 2, dim+1) second: two rows for combination
     # combination_table = n[combination_index, :].swapaxes(0, 1)
     # ND indexing is not supported in numba.
-    combination_table = np.empty((combination_index[0].shape[0], 2, ncols))
+    if combination_table_ is None:
+        combination_table: np.ndarray = np.empty((combination_index[0].shape[0], 2, ncols))
 
-    for i, (self_row_id, other_row_id) in enumerate(zip(*combination_index)):
-        combination_table[i, 0, :] = n[self_row_id, :]
-        combination_table[i, 1, :] = n[other_row_id, :]
+        for i, (self_row_id, other_row_id) in enumerate(zip(*combination_index)):
+            combination_table[i, 0, :] = n[self_row_id, :]
+            combination_table[i, 1, :] = n[other_row_id, :]
+    else:
+        combination_table = combination_table_
 
-    D = np.zeros((nrows, ))
+    D = np.zeros((nrows,))
 
     # shape: (n choose 2, dim + 1)
     B = np.empty((combination_table.shape[0], ncols))
@@ -132,4 +138,102 @@ def binary_agglomeration_jit(n, alpha: float, volume_fraction_powers: np.ndarray
         B[i, :] = volume_average_size_jit(agglomeration_parent_rows, volume_fraction_powers, shape_factor,
                                           per_particle=True)
         B[i, -1] = reduced
+
+    if compression:
+        partition_equivalent_rows_jit()
     return B, D
+
+
+def binary_agglomeration_parallel_wrapper(n, alpha: float, volume_fraction_powers: np.ndarray, shape_factor: float):
+    import threading
+    import os
+    ncpu = os.cpu_count()
+
+    nrows = n.shape[0]
+    per_cpu_rows = nrows / ncpu
+
+    combination_index = np.triu_indices(nrows, 1)
+    combination_table = n[combination_index, :].swapaxes(0, 1)
+
+    # trunk index
+    sub_index = []
+    sub_table = []
+    for i in range(0, len(combination_index[0]), per_cpu_rows):
+        sub_index.append((
+            combination_index[0][i: i + per_cpu_rows],
+            combination_index[1][i: i + per_cpu_rows]
+        ))
+        sub_table.append(combination_table[i: i + per_cpu_rows, :])
+
+@jit(nopython=True, cache=True, nogil=True)
+def compress_jit(n, volume_fraction_powers: np.ndarray, shape_factor: float, interval: float = 1e-6):
+    """
+    accelerated compression algorithm
+    TODO choose which dimension to adjust (adjust dim)
+    :param n:
+    :param interval:
+    :return:
+    """
+    adjust_dim = 0
+    ndims = n.shape[1] - 1
+
+    dims_min = np.empty((ndims,))
+    dims_max = np.empty((ndims,))
+
+    # store the group's count and volume in grid, so that no list of intermediate variable-size array will be created
+    for i in range(ndims):
+        col = n[:, i]
+        dims_min[i] = col.min()
+        dims_max[i] = col.max()
+    cnts = ((dims_max - dims_min) // interval + 1).astype(np.int_)
+    cumprod_dim = np.cumprod(np.hstack((np.array([1]), cnts)))[:-1]
+    # nd grid is flattened into 1d
+    # the first dimension is the slowest dimension.
+    volume_grid = np.zeros((np.prod(cnts),))
+    count_grid = np.zeros((np.prod(cnts),))
+    sum_non_adjust_dim_grid = np.zeros((np.prod(cnts), ndims - 1))
+
+    n_non_adjust_dim = np.delete(n.T, (adjust_dim, -1)).T
+
+    for i, (row, non_adjust_row) in enumerate(zip(n, n_non_adjust_dim)):
+        sizes = row[:-1]
+
+        # mapping sizes to indices
+        idx = (sizes - dims_min) // interval
+
+        # convert 2d index to 1d index
+        flat_idx = int((cumprod_dim * idx).sum())
+
+        vol = volume_fraction_jit(np.expand_dims(row, 0), volume_fraction_powers, shape_factor)
+        volume_grid[flat_idx] += vol
+        count_grid[flat_idx] += row[-1]
+
+        sum_non_adjust_dim_grid[flat_idx, :] += non_adjust_row
+
+    # remove the empty rows in the grid
+    empty_idx = count_grid == 0
+    volume_grid = volume_grid[~empty_idx]
+    count_grid = count_grid[~empty_idx]
+    sum_non_adjust_dim_grid = sum_non_adjust_dim_grid[~empty_idx]
+
+    # compute the equivalent size
+    if sum_non_adjust_dim_grid.size == 0:
+        # 1d
+        adjust_dim_size = (volume_grid / count_grid / shape_factor) ** (1 / volume_fraction_powers)
+        return np.hstack((adjust_dim_size.reshape((-1, 1)), count_grid.reshape((-1, 1))))
+    else:
+        mean_non_adjust_dim_grid = sum_non_adjust_dim_grid / count_grid.reshape((-1, 1))
+        non_adjust_power = np.delete(volume_fraction_powers, adjust_dim)
+        non_adjust_dim_vol_component = mean_non_adjust_dim_grid ** non_adjust_power
+
+        # product along rows to compute the volume from the component
+        non_adjust_dim_vol = np.ones((non_adjust_dim_vol_component.shape[0],))
+        for _, components in enumerate(non_adjust_dim_vol_component.T):
+            non_adjust_dim_vol *= components
+
+        adjust_dim_size = (volume_grid / count_grid / shape_factor / non_adjust_dim_vol) ** (1 / volume_fraction_powers[
+            adjust_dim])
+
+        # TODO: this step does not support adjust_dim
+        ret = np.hstack((adjust_dim_size.reshape((-1, 1)), mean_non_adjust_dim_grid, count_grid.reshape((-1, 1))))
+        return ret
--- tests/test_jit.py
+++ tests/test_jit.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 
 from crm.base.system_spec import FormSpec
-from crm.utils.jit import volume_average_size_jit, binary_agglomeration_jit
+from crm.utils.jit import volume_average_size_jit, binary_agglomeration_jit, compress_jit, volume_fraction_jit
 from crm.presets.hypothetical import Hypothetical1D, Hypothetical2D
 
 
@@ -17,6 +17,7 @@ def test_volume_average_size_jit():
     va_row = volume_average_size_jit(n_unity, form.volume_fraction_powers, form.shape_factor, 1)[np.newaxis, :]
     assert np.isclose(form.volume_fraction(va_row), form.volume_fraction(n_unity))
 
+
 def assert_volume_equal_after_agglomeration(n, B, D, form: FormSpec):
     n = n.copy()
     n[:, -1] = D
@@ -24,7 +25,6 @@ def assert_volume_equal_after_agglomeration(n, B, D, form: FormSpec):
     v2 = form.volume_fraction(B)
 
     assert np.isclose(v1, v2)
-
 
 
 def test_agglomeration_jit_1d():
@@ -36,17 +36,16 @@ def test_agglomeration_jit_1d():
     B, D = binary_agglomeration_jit(n, 1, form.volume_fraction_powers, form.shape_factor)
     assert_volume_equal_after_agglomeration(n, B, D, form)
 
-
     n = np.array([(1e-6, 10), (2e-6, 10), (6e-6, 52)])
 
     B, D = binary_agglomeration_jit(n, 1, form.volume_fraction_powers, form.shape_factor)
     assert_volume_equal_after_agglomeration(n, B, D, form)
 
-
     n = np.array([(1e-6, 10), (2e-6, 10), (6e-6, 52), (8e-6, 42)])
 
     B, D = binary_agglomeration_jit(n, 1, form.volume_fraction_powers, form.shape_factor)
     assert_volume_equal_after_agglomeration(n, B, D, form)
+
 
 def test_agglomeration_jit_2d():
     system_spec = Hypothetical2D()
@@ -56,6 +55,7 @@ def test_agglomeration_jit_2d():
 
     B, D = binary_agglomeration_jit(n, 1, form.volume_fraction_powers, form.shape_factor)
     assert_volume_equal_after_agglomeration(n, B, D, form)
+
 
 @pytest.mark.parametrize("system_spec_class", [Hypothetical1D, Hypothetical2D])
 @pytest.mark.parametrize("nrows", [100, 300, 1000])
@@ -70,3 +70,34 @@ def test_benchmark_agglomeration(nrows, system_spec_class, benchmark):
 
     B, D = benchmark(binary_agglomeration_jit, n, 1, form.volume_fraction_powers, form.shape_factor)
     assert_volume_equal_after_agglomeration(n, B, D, form)
+
+
+@pytest.mark.parametrize("nrows", [100, 1000, 10000, 100000, 1000000])
+@pytest.mark.parametrize("ndim", [1, 2, 3])
+@pytest.mark.parametrize("scale", [25e-6, 500e-6])
+def test_compression_jit(nrows, ndim, scale, benchmark, printer):
+    scale_size = scale
+    scale_count = 1e8
+    loc = scale * 2
+    sizes = np.random.normal(loc=loc, scale=scale_size, size=(nrows, ndim))
+    sizes = np.clip(sizes, loc - scale * 1.5, loc + scale * 1.5)
+    count = np.random.random((nrows, 1)) * scale_count
+    n = np.hstack((sizes, count))
+
+    if ndim == 1:
+        power = np.array([3])
+        shape_factor = 0.5
+    elif ndim == 2:
+        power = np.array([2, 1])
+        shape_factor = 0.5
+    elif ndim == 3:
+        power = np.array([1, 1, 1])
+        shape_factor = 0.5
+
+    result = benchmark(compress_jit, n, power, shape_factor)
+
+    original_volume = volume_fraction_jit(n, power, shape_factor)
+    result_volume = volume_fraction_jit(result, power, shape_factor)
+    assert np.isclose(original_volume, result_volume)
+
+    printer(f"compressed {nrows} to {result.shape[0]}")
